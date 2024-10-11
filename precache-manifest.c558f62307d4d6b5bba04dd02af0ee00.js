3a04a77049a989fca27012cb27565c1ae7d83e97
Dasep Moch Luay <dasepmoch@Laptop-Dasep.local>
2024-10-12 01:40:41 +0700

    Updates

---
+++ precache-manifest.c558f62307d4d6b5bba04dd02af0ee00.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e6b4d0b25ae654f7eac4d6fd925738b",
+    "url": "/CVArea/index.html"
+  },
+  {
+    "revision": "09707d4a23aae46dc23c",
+    "url": "/CVArea/static/css/2.fe35da1f.chunk.css"
+  },
+  {
+    "revision": "d025b2d12f4c5c82a4c5",
+    "url": "/CVArea/static/css/main.0a1c3ee2.chunk.css"
+  },
+  {
+    "revision": "09707d4a23aae46dc23c",
+    "url": "/CVArea/static/js/2.e0927b37.chunk.js"
+  },
+  {
+    "revision": "ad0e59f1c97dced6bfdb7baf2f4f95d9",
+    "url": "/CVArea/static/js/2.e0927b37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c15bd6d2414ce7e8ebd",
+    "url": "/CVArea/static/js/3.cd6bc728.chunk.js"
+  },
+  {
+    "revision": "9086e1c142ca8eb25f6e",
+    "url": "/CVArea/static/js/4.14aff801.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/CVArea/static/js/4.14aff801.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d2d94fffefa41fbb49d",
+    "url": "/CVArea/static/js/5.572265bc.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/CVArea/static/js/5.572265bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d025b2d12f4c5c82a4c5",
+    "url": "/CVArea/static/js/main.744b42d1.chunk.js"
+  },
+  {
+    "revision": "a679223b4ddcb3d4195a",
+    "url": "/CVArea/static/js/runtime-main.bc725252.js"
+  },
+  {
+    "revision": "0c571bfbfb5814de7c592683bb268a90",
+    "url": "/CVArea/static/media/1.0c571bfb.JPG"
+  },
+  {
+    "revision": "fba178679a94a1993bfda27c5edda53d",
+    "url": "/CVArea/static/media/2.fba17867.JPG"
+  },
+  {
+    "revision": "776a571459dad3cf206dbbb58b1f4775",
+    "url": "/CVArea/static/media/3.776a5714.JPG"
+  },
+  {
+    "revision": "af60bc02e18c70a997d54451f8638c8c",
+    "url": "/CVArea/static/media/4.af60bc02.jpg"
+  },
+  {
+    "revision": "fe67d6ad0f96005f0cda33a91e3bfba4",
+    "url": "/CVArea/static/media/5.fe67d6ad.JPG"
+  },
+  {
+    "revision": "b62f3e78b7ae1b62b8d6cb45056ceaf7",
+    "url": "/CVArea/static/media/6.b62f3e78.JPG"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/CVArea/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "1b5120a51a8a7b5a2bbb88ead0088cd0",
+    "url": "/CVArea/static/media/address.1b5120a5.svg"
+  },
+  {
+    "revision": "fa885f6f12620359516f542e227451c0",
+    "url": "/CVArea/static/media/address.fa885f6f.svg"
+  },
+  {
+    "revision": "ab78c4a1c304e26a7cae90bcdff3b9d2",
+    "url": "/CVArea/static/media/close.ab78c4a1.svg"
+  },
+  {
+    "revision": "3b978eab0b171f2e9ee5371d6a0002c9",
+    "url": "/CVArea/static/media/envelope.3b978eab.svg"
+  },
+  {
+    "revision": "4891f098d240d8fef3a6082796d096c3",
+    "url": "/CVArea/static/media/envelope.4891f098.svg"
+  },
+  {
+    "revision": "e44d4dd6594e6ae6718a89059f29cdc7",
+    "url": "/CVArea/static/media/facebook.e44d4dd6.svg"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/CVArea/static/media/getFetch.013f0b90.cjs"
+  },
+  {
+    "revision": "04f1d8728b781908558c8f1442c665ed",
+    "url": "/CVArea/static/media/instagram.04f1d872.svg"
+  },
+  {
+    "revision": "a450cc103b020589bf97577c53c009d7",
+    "url": "/CVArea/static/media/menu.a450cc10.svg"
+  },
+  {
+    "revision": "1c5073d33cfd02ef9cfc9fa8b9e1bea2",
+    "url": "/CVArea/static/media/phone-call.1c5073d3.svg"
+  },
+  {
+    "revision": "f57fd85218c3874da537e524a3a80711",
+    "url": "/CVArea/static/media/phone-call.f57fd852.svg"
+  },
+  {
+    "revision": "90ace9e6d1c7eddc36466c24a7a7bf59",
+    "url": "/CVArea/static/media/pinterest.90ace9e6.svg"
+  },
+  {
+    "revision": "5f4c69ad8a5a2bdff5c944577d530221",
+    "url": "/CVArea/static/media/preview.5f4c69ad.png"
+  },
+  {
+    "revision": "7e145c9eb60da95379f070be9920eca5",
+    "url": "/CVArea/static/media/twitter.7e145c9e.svg"
+  },
+  {
+    "revision": "9082c49f388c746aa2c6ac98be3f39f8",
+    "url": "/CVArea/static/media/undraw_add_document_0hek.9082c49f.svg"
+  },
+  {
+    "revision": "0f519090d00fbd2b11105f33df82a056",
+    "url": "/CVArea/static/media/youtube.0f519090.svg"
+  }
+]);
